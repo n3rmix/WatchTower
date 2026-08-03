d3c47ae6faddd6bf6624d183c451449dc2055bae
n3rmix <nermix@web.de>
2026-08-03 13:48:57 +0200

    debug

--- frontend/src/components/HumanitarianThemeRadar.js
+++ frontend/src/components/HumanitarianThemeRadar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
 import axios from "axios";
 import { Heart, ChevronDown } from "lucide-react";
 
